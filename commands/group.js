c172775a2cd7313c1f6d435cb5dc291757264116
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-01-29 15:25:18 +0530

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -212,9 +212,6 @@ cmd({
         if (!citel.isGroup) return citel.reply(tlang().group);
         const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat).catch((e) => {}) : "";
         const participants = citel.isGroup ? await groupMetadata.participants : "";
-        const groupAdmins = await getAdmin(Void, citel)
-        const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
-        if (!isAdmins) return citel.reply(tlang().admin);
 
         let textt = `
 ══✪〘   *Tag All*   〙✪══
